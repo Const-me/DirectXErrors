eeebbf89d205e8ab56d6778d5bcb8cdabc2bcd9c
Konstantin <soonts@live.com>
2018-09-06 17:25:49 +0200

    Windows 8.1 SDK support

--- DirectXErrors/DirectXErrors/DirectXErrors.cpp
+++ DirectXErrors/DirectXErrors/DirectXErrors.cpp
@@ -32,11 +32,20 @@
 
 #include "codes.h"
 
+namespace
+{
+	// The built-in HRESULT_FROM_WIN32 is an inline function without constexpr in Win8.1 SDK. Need to workaround.
+	constexpr HRESULT hresultFromWin32( unsigned long x )
+	{
+		return (HRESULT)( x ) <= 0 ? (HRESULT)( x ) : (HRESULT)( ( ( x ) & 0x0000FFFF ) | ( FACILITY_WIN32 << 16 ) | 0x80000000 );
+	}
+}
+
 const char* getDxErrorStringA( HRESULT hr )
 {
 #define CHK_ERR( code, description )      case code: return #code;
 #define CHK_ERRA( code )                  case code: return #code;
-#define CHK_ERR_WIN32A( code )            case HRESULT_FROM_WIN32( code ) : return #code;
+#define CHK_ERR_WIN32A( code )            case hresultFromWin32( code ) : return #code;
 	switch( hr )
 	{
 #include "data/win32.inl"
@@ -52,7 +61,7 @@ const wchar_t* getDxErrorStringW( HRESULT hr )
 {
 #define CHK_ERR( code, description )      case code: return L#code;
 #define CHK_ERRA( code )                  case code: return L#code;
-#define CHK_ERR_WIN32A( code )            case HRESULT_FROM_WIN32( code ) : return L#code;
+#define CHK_ERR_WIN32A( code )            case hresultFromWin32( code ) : return L#code;
 	switch( hr )
 	{
 #include "data/win32.inl"
